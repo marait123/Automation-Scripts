f8e55cafc285c70291ae413033cecc260c5c1202
marait123 <max.ibrahim43@hotmail.com>
2021-02-22 17:46:58 +0200

    finshed pdf-count which counts the number of pdf files pages

---
+++ pdfs-count.py
@@ -0,0 +1,19 @@
+import os
+import PyPDF2
+folder_path = "D:\\3rd-year-1st-semster\\Algorithms\\lectures"
+files = os.listdir(folder_path)
+os.chdir(folder_path)
+print(files)
+# importing required modules
+pages = 0
+for pdf in files:
+    # creating a pdf file object
+    pdfFileObj = open(pdf, 'rb')
+    # creating a pdf reader object
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # printing number of pages in pdf file
+    print("pdf name: ", pdf, " page_count: ", pdfReader.numPages)
+    pages += pdfReader.numPages
+    # closing the pdf file object
+    pdfFileObj.close()
+print("total number of pages is ", pages)
